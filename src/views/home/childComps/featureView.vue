001d6e4adedfdcca0e1adfe7e9c92ec0f110bf42
wszlgn <936270577@qq.com>
2021-08-07 00:09:56 +0800

    featureView finish

---
+++ src/views/home/childComps/featureView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="feature">
+      <a href="https://act.mogujie.com/zzl1x67">
+        <img src="~assets/img/home/recommend_bg.jpg" alt="">
+      </a>
+    </div>
+</template>
+<script>
+export default {
+  name: 'FeatureView',
+   data() {
+      return {
+      }
+   },
+   activated() {
+   },
+ watch: {
+},
+created(){
+},
+mounted(){
+},
+methods:{
+}
+
+
+}
+</script>
+<style scoped>
+  #feature img {
+    width: 100%;
+  }
+</style>
